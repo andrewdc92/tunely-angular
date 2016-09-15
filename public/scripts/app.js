33512a5988d277be67b0b455c0bd198f4e40ffb0
Andrew Cordivari <andrewcord@William-Cordivaris-MacBook-Pro.local>
2016-09-15 13:45:31 -0700

    completely lost starting sprint 3, just tried to copy projector and did not keep up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,34 +4,77 @@
  *
  */
 
-angular
+
+ angular
   .module('tunely', [])
-  .controller('AlbumsIndexController', AlbumsIndexController);
-  // ^ the first argument is a string naming the controller,
-  // the second argument is a function that defines the capacities
-  // of the controller.
+  .controller('AlbumsIndexController', AlbumsIndexController)
 
-function AlbumsIndexController () {
-  var vm = this;
-  vm.newAlbum = {};
+  AlbumsIndexController.$inject = ['$http'];
+  function AlbumsIndexController ( $http ) {
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
+    var vm = this;
+    vm.newAlbum = {};
+
+    vm.newAlbum = {
+      name: 'enter album name',
+      artistName: 'enter artist name'
+    };
+
+    vm.newAlbum = {
+      name: 'License to Ill',
+      artistName: 'Beastie Boys'
+    };
+
+    vm.albums = [];
+
+        $http({
+          method: 'GET',
+          url: '/api/albums'
+        }).then(function successCallback(response) {
+          console.log('response for all projects:', response)
+          vm.albums = response.data;
+        }, function errorCallback(response) {
+          console.log('There was an error getting the data', response);
+        });
+      }
+      vm.getAlbums();
+
+      vm.createAlbum = function() {
+        http({
+          method: 'POST',
+          url: '/api/albums',
+          data: vm.newAlbum.name,
+          artistName: vm.newAlbum.artistName
+        }).then(function sucessCallback(response) {
+          console.log(response.data);
+          vm.albums.push(response.data)
+          vm.newAlbum = {
+            name: "",
+            artistName: ""
+          }
+
+        }, function errorCallback(response) {
+          console.log('There was an error getting the data')
+        }
+        })
+      }
     }
-  ];
-}
+
+
+
+  //   vm.albums = [
+  //     {
+  //       name: 'Coming Home',
+  //       artistName: 'Leon Bridges'
+  //     },
+  //     {
+  //       name: 'Are We There',
+  //       artistName: 'Sharon Van Etten'
+  //     },
+  //     {
+  //       name: 'The Queen is Dead',
+  //       artistName: 'The Smiths'
+  //     }
+  //   ];
+  // }
